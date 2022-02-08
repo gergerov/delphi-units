6c0b3d356df6a960a9dcc7931d5a2da21f85448c
Gergerov <gergerov94@gmail.com>
2022-02-08 23:08:48 +0300

    -

--- uCustomUser.pas
+++ uCustomUser.pas
@@ -15,31 +15,46 @@ type
 type
   TADOCustomUser = class(TInterfacedObject, ICustomUserAuth)
     public
-
       FID: integer;
-      FUSERNAME: string;
-      FisAUTH: boolean;
-      FConnection: TADOConnection;
-      constructor Create(pUsername: string; pPassword: string; pConnection: TADOConnection);
+      constructor Create;
       procedure Auth;
 
+      procedure setConnectionString(Value: string);
+      function getConnection: TADOConnection;
     private
       FPassword: string; // хранится только хэшированный
+      FUSERNAME: string;
+      FAuthStatus: boolean;
+      FConnection: TADOConnection;
+      FConnectionString: string;
+
+      procedure setAuthData(pUsername: string; pPassword: string);
+      procedure setAuth(Value: boolean);
+    published
+      property Password: string read FPassword write FPassword;
+      property Username: string read FUSERNAME write FUSERNAME;
+      property isAuth: boolean read FAuthStatus write setAuth;
+      property ID: integer read FID write FID;
+      property ConnectionString: string read FConnectionString;
   end;
 
 Type
   TCTMUser = class(TADOCustomUser)
-    procedure Auth;
+    procedure Auth(pUsername: string; pPassword: string);
 
   end;
 
 implementation
 
-constructor TADOCustomUser.Create(pUsername: string; pPassword: string; pConnection: TADOConnection);
+  { ---------------CUSTOM USER------------------------------ }
+constructor  TADOCustomUser.Create;
   begin
-    FUSERNAME := pUsername;
-    FPassword := THashMD5.GetHashString(pPassword);
-    FConnection := pConnection;
+
+  end;
+
+procedure TADOCustomUser.setConnectionString(Value: string);
+  begin
+    FConnectionString := Value;
   end;
 
 procedure TADOCustomUser.Auth;
@@ -47,12 +62,54 @@ procedure TADOCustomUser.Auth;
 //    LoginInDB;
   end;
 
-procedure TCTMUser.Auth;
+procedure TADOCustomUser.setAuth(Value: boolean);
+  begin
+    if not Value then
+      // очистим
+      begin
+        FPassword := '';
+        FUsername := '';
+        FID := 0;
+//        FConnection.Destroy;
+        Fconnection := Nil;
+        FAuthStatus := Value;
+      end
+    else
+      begin
+        FConnection := TADOConnection.Create(NIL);
+        FConnection.CommandTimeout := 5;
+        FConnection.ConnectionTimeout := 5;
+        FConnection.ConnectionString := FConnectionString;
+        FConnection.Connected := True;
+      end;
+
+    FAuthStatus := Value;
+  end;
+
+procedure TADOCustomUser.setAuthData(pUsername: string; pPassword: string);
+  begin
+    Username := pUsername;
+    Password := THashMD5.GetHashString(pPassword);
+  end;
+
+function TADOCustomUser.getConnection: TADOConnection;
+  begin
+    result := FConnection;
+  end;
+
+  { --------------CTM USER ----------------------------------}
+//CTMUser.setConnectionString(ConnectionSetup.GetConnectionString(cbIniSection.Selected.Text));
+//CTMUser.Auth(editUsername.Text, editPassword.Text);
+procedure TCTMUser.Auth(pUsername: string; pPassword: string);
   var
     q: TADOQuery;
   begin
+    setAuthData(pUsername, pPassword);
     q := TADOQuery.Create(NIL);
-    q.Connection := FConnection;
+    q.Connection := TADOConnection.Create(NIL);
+    q.Connection.ConnectionTimeout := 5;
+    q.Connection.CommandTimeout := 5;
+    q.Connection.ConnectionString := FConnectionString;
     q.SQL.Clear;
     q.SQL.Add(
       'exec CTMUsers#login '
@@ -64,11 +121,14 @@ procedure TCTMUser.Auth;
     if q.RecordCount = 1 then
       begin
         FID := q.FieldValues['ID'];
-        FisAUTH := True;
-        q.Close;
-        Exit;
+//        ShowMessage(IntToStr(FID));
+        setAuth(True);
+      end
+    else
+      begin
+        setAuth(False);
+//        ShowMessage(q.SQL[0]);
       end;
-    FisAUTH := False;
-    exit;
   end;
+
 end.
